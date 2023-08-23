a8d7c4c186f97d5e24319b1e2a76c779fcbd2a02
Manuel <milton.sanchezrios88@gmail.com>
2023-08-22 22:31:18 -0500

    advances

--- src/tests/purchase.test.js
+++ src/tests/purchase.test.js
@@ -3,6 +3,8 @@ const app = require("../app")
 const Product = require("../models/Product")
 const Cart = require("../models/Cart")
 
+require("../models/")
+
 
 const URL_BASE_USERS = "/api/v1/users"
 const URL_BASE = "/api/v1/purchase"
@@ -58,3 +60,20 @@ test("POST -> 'URL_BASE', should return status code 201 and res.body.quantiy ===
     expect(res.body[0].quantity).toBe(bodyCart.quantity)
 
 });
+test("GET -> 'URL_BASE',should return status code 200 res.body.length === 1", async () => {
+
+    const res = await request(app)
+      .get(URL_BASE)
+      .set("Authorization", `Bearer ${TOKEN}`)
+  
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+    expect(res.body[0].userId).toBe(userId)
+    expect(res.body[0].product).toBeDefined()
+    expect(res.body[0].product.id).toBe(product.id)
+    expect(res.body[0].product.productImgs).toBeDefined()
+    expect(res.body[0].product.productImgs).toHaveLength(0)
+  
+    await product.destroy()
+  })
